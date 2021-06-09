e2cf8ac0de269fb42d8c1f77c3137b336cc07d6e
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-06-09 17:41:15 +0530

    Wrapping Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductDetail from './screens/ProductDetail'
+
 function App() {
   return (
-    <div>
+    <Router>
     <Header/>
-    Hello
-    <main className = "py-5">
-    <Container>
-    <h1>Hey</h1>
-    </Container>
-    </main>
+      <main className = "py-5">
+      <Container>
+        <Route path = '/' component={ HomeScreen}  exact />
+        <Route path = '/product/:id' component={ProductDetail}/>
+      </Container>
+      </main>
     <Footer/>
-    </div>
+    </Router>
+  
   );
 }
 
---
+++ frontend/src/screens/ProductDetail.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Col,Row,ListGroup,Image,Button,Card} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import Rating from '../components/Rating'
+import products from '../products'
+function ProductDetail({ match }) {
+    const productdetail = products.find((p) => p._id == match.params.id)
+    return (
+        <div>
+            <Link to='/' className="btn btn-light my-3">Go Back</Link>
+            <Row>
+                <Col md = {6}>
+                    < Image src ={productdetail.image} alt = {productdetail.name} fluid />
+                </Col>
+                <Col md = {3}>
+                    <ListGroup variant = 'flush'>
+                        <ListGroup.Item>
+                            <h3>{productdetail.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value = {productdetail.rating} text = {`${productdetail.numReviews} reviews`} color = {"#f8e825"} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price : ${productdetail.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {productdetail.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md= {3}>
+                    <Card>
+                        <ListGroup variant = 'flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col>${productdetail.price}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Stock:</Col>
+                                    <Col>{productdetail.countInStock > 0 ? "In Stock": "Out Of Stock"} </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className = "btn-block" id = "addtocart" type = "button" disabled = {productdetail.countInstock==0}>Add To Cart</Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default ProductDetail
