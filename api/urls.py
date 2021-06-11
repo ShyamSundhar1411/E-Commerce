7b65e4da0c3e849930f263a2a102588dd2e8604a
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-06-11 22:12:53 +0530

    Backend Authentication

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
+    #Products
     path('',views.getRoutes,name = "routes"),
     path('products',views.getProducts,name = "products"),
-    path('products/<str:pk>',views.getProduct,name = "product")
+    path('products/<str:pk>',views.getProduct,name = "product"),
+    #Users
+    path('users',views.getUsers,name = "users"),
+    path('users/register',views.registerUser,name = "registerUser"),
+    path('users/login', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/',views.getUserProfile,name = "userprofile"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,60 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
-from . serializers import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
+from . serializers import ProductSerializer,UserSerializer,UserSerializerWithToken
 from . models import Product
+
 # Create your views here.
+#Class Based Views
+class CustomTokenPairSerializer(TokenObtainPairSerializer):
+    def validate(self,attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+        for i,v in serializer.items():
+            data[i] = v
+        return data
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenPairSerializer
+#Function Based Views
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password'])
+            )
+        serializer = UserSerializerWithToken(user,many = False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exists'}
+        return Response(message,status = status.HTTP_404_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many = True)
+    return Response(serializer.data)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user,many = False)
+    return Response(serializer.data)
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/products',
+        '/api/users',
         '/api/products/<id>',
         '/api/products/reviews',
         '/api/products/top',
@@ -15,6 +62,9 @@ def getRoutes(request):
         '/api/products/<id>/delete',
         '/api/products/create',
         '/api/products/upload',
+        '/api/users/register'
+        '/api/users/login',
+        '/api/users/profile/',
     ]
     return Response(routes)
 @api_view(['GET'])
