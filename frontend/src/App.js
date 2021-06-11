c8da518ac0f15718a5d6036a69a246b45439c21b
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2021-06-11 13:15:46 +0530

    Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
 import ProductDetail from './screens/ProductDetail'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
@@ -13,11 +14,12 @@ function App() {
       <Container>
         <Route path = '/' component={ HomeScreen}  exact />
         <Route path = '/product/:id' component={ProductDetail}/>
+        <Route path = '/Cart/:id?' component={CartScreen}/>
       </Container>
       </main>
     <Footer/>
     </Router>
-  
+
   );
 }
 
--- frontend/src/screens/ProductDetail.js
+++ frontend/src/screens/ProductDetail.js
@@ -1,63 +1,97 @@
 import React, {useEffect,useState} from 'react'
-import {Col,Row,ListGroup,Image,Button,Card} from 'react-bootstrap'
+import {Col,Row,ListGroup,Form,Image,Button,Card} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import Rating from '../components/Rating'
-import axios from 'axios'
-function ProductDetail({ match }) {
-    const [productdetail, setProduct] = useState([])
-    useEffect(() =>{
-      async function fetchProduct(){
-        const { data } = await axios.get(`/api/products/${match.params.id}`)
-        setProduct(data)
-      }
-      fetchProduct()
-    },[])
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import {useDispatch,useSelector} from 'react-redux'
+import {listProductDetails} from '../actions/productAction'
+function ProductDetail({ match,history }) {
+    const dispatch = useDispatch()
+    const [qty,setQty] = useState(1)
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading,error,productdetail} = productDetails
+    useEffect(() => {
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch])
+    const addtoCartFunction = () => {
+      history.push(`/Cart/${match.params.id}?qty=${qty}`)
+
+    }
     return (
         <div>
             <Link to='/' className="btn btn-light my-3">Go Back</Link>
-            <Row>
-                <Col md = {6}>
-                    < Image src ={productdetail.image} alt = {productdetail.name} fluid />
-                </Col>
-                <Col md = {3}>
-                    <ListGroup variant = 'flush'>
-                        <ListGroup.Item>
-                            <h3>{productdetail.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value = {productdetail.rating} text = {`${productdetail.numReviews} reviews`} color = {"#f8e825"} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Price : ${productdetail.price}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {productdetail.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md= {3}>
-                    <Card>
+            {loading ? <Loader/>
+              : error ? <Message variant = 'danger'>{error}</Message>
+                :<Row>
+                    <Col md = {6}>
+                        <Image src ={productdetail.image} alt = {productdetail.name} fluid />
+                    </Col>
+                    <Col md = {3}>
                         <ListGroup variant = 'flush'>
                             <ListGroup.Item>
-                                <Row>
-                                    <Col>Price:</Col>
-                                    <Col>${productdetail.price}</Col>
-                                </Row>
+                                <h3>{productdetail.name}</h3>
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Row>
-                                    <Col>Stock:</Col>
-                                    <Col>{productdetail.countInStock > 0 ? "In Stock": "Out Of Stock"} </Col>
-                                </Row>
+                                <Rating value = {productdetail.rating} text = {`${productdetail.numReviews} reviews`} color = {"#f8e825"} />
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Button className = "btn-block" id = "addtocart" type = "button" disabled = {productdetail.countInstock==0}>Add To Cart</Button>
+                                Price : ${productdetail.price}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                Description: {productdetail.description}
                             </ListGroup.Item>
                         </ListGroup>
-                    </Card>
+                    </Col>
+                    <Col md= {3}>
+                        <Card>
+                            <ListGroup variant = 'flush'>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Price:</Col>
+                                        <Col>${productdetail.price}</Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Status:</Col>
+                                        <Col>{productdetail.countInStock > 0 ? "In Stock": "Out Of Stock"} </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                {productdetail.countInStock > 0 &&(
+                                  <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col xs = 'auto' className = 'my-1'>
+                                          <Form.Control as = 'select' value = {qty}
+                                          onChange = {(e) => setQty(e.target.value)}>
 
-                </Col>
-            </Row>
+                                          {
+                                            [...Array(productdetail.countInStock).keys()].map((x) => (
+                                              <option key = {x+1} value = {x+1}>
+                                                {x+1}
+                                              </option>
+                                            ))
+                                          }
+                                          </Form.Control>
+                                        </Col>
+
+
+
+                                    </Row>
+                                  </ListGroup.Item>
+                                )}
+                                <ListGroup.Item>
+                                    <Button className = "btn-block" id = "addtocart" type = "button" disabled = {productdetail.countInstock==0}
+                                    onClick = {addtoCartFunction}>
+                                    Add To Cart</Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+
+                    </Col>
+                </Row>}
+
         </div>
     )
 }
